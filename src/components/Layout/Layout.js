98c46c4a2b7ad8a0fa6faa61f4bbc6733bc9f205
Emre <hsnemrecetin@gmail.com>
2019-11-29 22:45:27 +0300

    Sidedrawer component created

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -3,11 +3,12 @@ import React from 'react';
 import Wrapper from '../../hoc/Wrapper';
 import classes from './Layout.module.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
+import Sidedrawer from '../Navigation/SideDrawer/SideDrawer';
 
 const layout = (props) => (
   <Wrapper>
     <Toolbar/>
-    <div> Toolbar, Sidedrawer, Backdrop</div>
+    <Sidedrawer/>
     <main className={classes.Content}>
         {props.children}
     </main>
--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -4,6 +4,10 @@ import burgerLogo from '../../assets/images/burger-logo.png';
 import classes from './Logo.module.css';
 
 const logo = (props) => (
+    // We can set style dynamically because of logo component will use in both of
+    // sidedrawer and the toolbar with different heights. 
+    // But we used different approch for this. Instead of setting the props dynamicly,
+    // we used css for <div> sections with the same .Logo component 
     <div className={classes.Logo}>
         <img src={burgerLogo} alt="MyBurger"/>
     </div>
