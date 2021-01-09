d51664e8fb51886066e5b4002c888848476205f0
Ali <alicetin.dev@gmail.com>
2021-01-09 20:05:33 +0300

    OrderSum changed for performance, (render methods has been used more wisely now)

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,30 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Wrapper from '../../../hoc/Wrapper';
 import Button from '../../UI/Button/Button'
 
-const orderSummary = (props) => {
-    const ingredientSummary = Object.keys(props.ingredients)
+class OrderSummary extends Component {
+    componentWillUpdate(){
+        console.log("Update from OrderSummary!")
+    };
+
+    render(){
+        const ingredientSummary = Object.keys(this.props.ingredients)
                 .map(igKey => {
                 return (
                     <li key={igKey}>
-                        <span style={{textTransform: 'capitalize'}}>{igKey}</span> : {props.ingredients[igKey]}
+                        <span style={{textTransform: 'capitalize'}}>{igKey}</span> : {this.props.ingredients[igKey]}
                     </li> );
                 });
+        return(
+            <Wrapper>
+                <h3>Your Order</h3>
+                <p>A delicious burger with the following ingredients:</p>
+                <ul>
+                    {ingredientSummary}
+                </ul>
+                <p><strong>Total Price: {this.props.price.toFixed(2)}</strong></p>
+                <p>Continue or Checkout?</p>
+                <Button btnType="Danger" clicked={this.props.puchaseCanceledEvent}>Cancel</Button>
+                <Button btnType="Success" clicked={this.props.puchaseContinueEvent}>Continue</Button>
+            </Wrapper>
 
-    return (
-        <Wrapper>
-            <h3>Your Order</h3>
-            <p>A delicious burger with the following ingredients:</p>
-            <ul>
-                {ingredientSummary}
-            </ul>
-            <p><strong>Total Price: {props.price.toFixed(2)}</strong></p>
-            <p>Continue or Checkout?</p>
-            <Button btnType="Danger" clicked={props.puchaseCanceledEvent}>Cancel</Button>
-            <Button btnType="Success" clicked={props.puchaseContinueEvent}>Continue</Button>
-        </Wrapper>
-    );
+        );
+    }
 };
 
-export default orderSummary;
+export default OrderSummary;
--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,21 +1,25 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 import classes from './Model.module.css';
 import Wrapper from '../../../hoc/Wrapper';
 import Backdrop from '../Backdrop/Backdrop';
 
-const modal = (props) => (
-    <Wrapper>
-        <Backdrop show={props.show} clicked={props.modalClosed}/>
-        <div 
-            className={classes.Modal}
-            style={{
-                transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-                opacity: props.show ? '1' : '0'
-        }}>
-            {props.children}
-        </div>
-    </Wrapper>
-);
+class Modal extends Component {
+    render(){
+        return(
+        <Wrapper>
+            <Backdrop show={this.props.show} clicked={this.props.modalClosed}/>
+            <div 
+                className={classes.Modal}
+                style={{
+                    transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+                    opacity: this.props.show ? '1' : '0'
+            }}>
+                {this.props.children}
+            </div>
+        </Wrapper>
+        );
+    }
+};
 
-export default modal;
+export default Modal;
